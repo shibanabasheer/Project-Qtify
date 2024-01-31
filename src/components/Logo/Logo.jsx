5586f5b122e62a15b393f2b7a031a890b98ff1f0
shibanabasheer <shibana2209@gmail.com>
2024-01-31 20:16:53 +0530

    test commit

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import LogoImage from "../../assets/Group 1.png";
 
 const Logo = () => {
-    return(
-        <img src={LogoImage} alt="logo" width={67}/>
+    return (
+        <img src={LogoImage} alt="logo" width={67} />
     )
 }
 
