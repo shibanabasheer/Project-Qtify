af4b2a1d47cb88f9ea08f444b5a09fe29da8aa25
shibanabasheer <shibana2209@gmail.com>
2024-01-31 19:04:56 +0530

    test commit

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import LogoImage from "../../assets/Group 1.png";
-import styles from "./Logo.module.css"
 
 const Logo = () => {
     return(
-        <div className={styles.logo}>
+        <div>
            <img src={LogoImage} alt="logo" width={67} /> 
         </div>
         
