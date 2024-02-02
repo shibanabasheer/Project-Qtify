5d835874d4aedec17dec1fc477ce9b1b028f891f
shibanabasheer <shibana2209@gmail.com>
2024-02-02 17:32:18 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
+import { fetchTopAlbums } from "./api/api";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Section from "./components/Section/Section";
+import styles from "./App.module.css"
 
 function App() {
+  const [topAlbumsData,setTopAlbumsData] = useState([]);
+
+  const generateTopAlbums = async () => {
+    try{
+      const data = await fetchTopAlbums();
+      setTopAlbumsData(data);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    generateTopAlbums();
+  },[])
+  
   return (
     <div>
       <Navbar />
       <Hero />
+      {/* {
+        topAlbumsData.map((topAlbum) => (
+          <Card data={topAlbum} type="album" key={topAlbum.id} />
+        ))
+      } */}
+      <div className={styles.sectionWrapper}>
+        <Section data={topAlbumsData} title="Top Albums" type="album"/>
+      </div>
     </div>
   );
 }
